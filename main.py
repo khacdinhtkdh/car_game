6f3fc2237f046a5bdbb39fea935d0beca9bb3864
khacdinhtkdh <khacdinhtkdh@gmail.com>
2022-05-09 20:16:21 +0700

    car main

--- main.py
+++ main.py
@@ -18,9 +18,17 @@ def move_car(cars):
             new_cars.append(car)
     return new_cars
 
+
 def draw_cars(cars):
     for car in cars:
         screen.blit(car_surface, car)
+
+
+def check_collision(cars):
+    for car in cars:
+        if car_main.colliderect(car):
+            return False
+    return True
 
 
 # Intialize the pygame
@@ -34,16 +42,18 @@ clock = pygame.time.Clock()
 background = pygame.image.load('images/background1.png').convert()
 background = pygame.transform.scale2x(background)
 
-
 car_surface = pygame.image.load('images/car.png').convert()
 # car_surface = pygame.transform.scale2x(car_surface)
 car_list = []
+pos_x = [110, 170, 230, 290]
+index_car = 2
 
+car_main_surface = pygame.image.load('images/car_main.png').convert()
+car_main = car_main_surface.get_rect(midtop=(pos_x[index_car], 500))
 # timer cars
 car_time = pygame.USEREVENT
 pygame.time.set_timer(car_time, 2500)
 
-pos_x = [110, 170, 230, 290]
 # # Sound
 # mixer.music.load("background.wav")
 # mixer.music.play(-1)
@@ -61,9 +71,22 @@ while True:
         if event.type == car_time:
             car_list.append(create_car())
 
-    screen.blit(background, (0, 0))
-    # screen.blit(car, (100, 100))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                index_car = index_car - 1
+            if event.key == pygame.K_RIGHT:
+                index_car = index_car + 1
 
+    if index_car < 0:
+        index_car = 0
+    if index_car > 3:
+        index_car = 3
+
+    screen.blit(background, (0, 0))
+    if not check_collision(car_list):
+        print('aaaaaaa')
+    car_main.centerx = pos_x[index_car]
+    screen.blit(car_main_surface, car_main)
     car_list = move_car(car_list)
     draw_cars(car_list)
 
